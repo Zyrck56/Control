d222c378e7e57040a8fb12777a4dba75f1c7b0a4
Zyrck56 <tomxr@msn.com>
2017-09-01 17:22:37 -0600

    vertical sliders initd

--- app/src/main/java/com/thomasghagen/control/ControlActivity.java
+++ app/src/main/java/com/thomasghagen/control/ControlActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class ControlActivity extends AppCompatActivity {
 
@@ -15,10 +16,52 @@ public class ControlActivity extends AppCompatActivity {
         setContentView(R.layout.activity_control);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        VerticalSeekBar leftseekbar = (VerticalSeekBar)findViewById(R.id.leftseekbar);
+        VerticalSeekBar rightseekbar = (VerticalSeekBar)findViewById(R.id.rightseekbar);
+
+        leftseekbar.setProgress(50);
+        rightseekbar.setProgress(50);
+
+        leftseekbar.setOnSeekBarChangeListener(leftlistener);
+        rightseekbar.setOnSeekBarChangeListener(rightlistener);
         
 
         
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    VerticalSeekBar.OnSeekBarChangeListener leftlistener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+    VerticalSeekBar.OnSeekBarChangeListener rightlistener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
 }
