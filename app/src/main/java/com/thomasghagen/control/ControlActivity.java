6a3b8bae33f992d7e1909ed1185e054282a05f55
Zyrck56 <tomxr@msn.com>
2017-09-01 18:14:39 -0600

    Works but does not snap

--- app/src/main/java/com/thomasghagen/control/ControlActivity.java
+++ app/src/main/java/com/thomasghagen/control/ControlActivity.java
@@ -1,14 +1,25 @@
 package com.thomasghagen.control;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class ControlActivity extends AppCompatActivity {
+    String S_ipaddress;
+    String S_port;
+    int I_port;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +27,11 @@ public class ControlActivity extends AppCompatActivity {
         setContentView(R.layout.activity_control);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        SharedPreferences socketinfo = getSharedPreferences("F_socketinfo", 0);
+        S_ipaddress = socketinfo.getString("ipaddress", "192.168.0.0");
+        S_port = socketinfo.getString("port", "0");
+        I_port = Integer.parseInt(S_port);
 
         VerticalSeekBar leftseekbar = (VerticalSeekBar)findViewById(R.id.leftseekbar);
         VerticalSeekBar rightseekbar = (VerticalSeekBar)findViewById(R.id.rightseekbar);
@@ -34,7 +50,27 @@ public class ControlActivity extends AppCompatActivity {
     VerticalSeekBar.OnSeekBarChangeListener leftlistener = new SeekBar.OnSeekBarChangeListener() {
         @Override
         public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            final String outgoingMessage = "Left: " + Integer.toString(i);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Socket client = new Socket(S_ipaddress, I_port);
+                        DataOutputStream dataOutputStream = new DataOutputStream(client.getOutputStream());
+                        dataOutputStream.writeUTF(outgoingMessage);
 
+                        DataInputStream dataInputStream = new DataInputStream(client.getInputStream());
+                        String S_incomingMessage = dataInputStream.readUTF();
+                        Log.i("incoming message", S_incomingMessage);
+
+                        client.close();
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
         }
 
         @Override
@@ -44,14 +80,34 @@ public class ControlActivity extends AppCompatActivity {
 
         @Override
         public void onStopTrackingTouch(SeekBar seekBar) {
-
+            seekBar.setProgress(50);
         }
     };
 
     VerticalSeekBar.OnSeekBarChangeListener rightlistener = new SeekBar.OnSeekBarChangeListener() {
         @Override
         public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            final String outgoingMessage = "Right: " + Integer.toString(i);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Socket client = new Socket(S_ipaddress, I_port);
+                        DataOutputStream dataOutputStream = new DataOutputStream(client.getOutputStream());
+                        dataOutputStream.writeUTF(outgoingMessage);
 
+                        DataInputStream dataInputStream = new DataInputStream(client.getInputStream());
+                        String S_incomingMessage = dataInputStream.readUTF();
+                        Log.i("incoming message", S_incomingMessage);
+
+                        client.close();
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
         }
 
         @Override
@@ -59,9 +115,10 @@ public class ControlActivity extends AppCompatActivity {
 
         }
 
-        @Override
+
         public void onStopTrackingTouch(SeekBar seekBar) {
 
+            Log.i("test", Integer.toString(seekBar.getProgress()));
         }
     };
 }
